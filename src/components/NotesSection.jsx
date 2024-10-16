e80a562cd67e826dd7b225e30e4ad146ada1098f
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2024-10-16 23:59:25 +0530

    UI Changes

--- src/components/NotesSection.jsx
+++ src/components/NotesSection.jsx
@@ -4,25 +4,35 @@ import "../App.css";
 const NotesSection = forwardRef((props, ref) => {
   return (
     <div className="notesSection-container" ref={ref}>
-      <nav className="navbar">
-        <div className="nav-items">
-          <ul>
-            <li><button>Home</button></li>
-            <li><button>Notes</button></li>
-            <li><button>Saved</button></li>
-            <li><button>Collection</button></li>
-          </ul>
-        </div>
-      </nav>
+      <aside className="sidebar">
+        <h2>Folders</h2>
+        <ul>
+          <li>Personal</li>
+          <li>Work</li>
+          <li>Projects</li>
+          <li>Ideas</li>
+          <li>Task</li>
+        </ul>
+      </aside>
       <div className="main-content">
-        <aside className="sidebar">
-          <h2>Folders</h2>
-          <ul>
-            <li>Personal</li>
-            <li>Work</li>
-            <li>Ideas</li>
-          </ul>
-        </aside>
+        <nav className="navbar">
+          <div className="nav-items">
+            <ul>
+              <li>
+                <button>Home</button>
+              </li>
+              <li>
+                <button>Notes</button>
+              </li>
+              <li>
+                <button>Saved</button>
+              </li>
+              <li>
+                <button>Collection</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
         <main className="note-area">
           <h2>Welcome to Noted</h2>
           <p>This is where you can add, edit, and manage your notes.</p>
