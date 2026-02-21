24497619a97645fa9a01d2432a6c9ce9ac2d364e
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2026-02-22 02:07:53 +0530

    Fixed the routing issue which prevented the main component of the app i.e LayoutShell to render.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,20 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import LayoutShell from "@/components/layout/LayoutShell";
 import Intro from "../pages/Intro";
-// import Board from "@/components/canvas/Board";
-// import PageTransition from '../components/animations/PageTransition'
 import Error from "../components/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Intro />,
-    children: [
-      {
-        path: "main",
-        element: <LayoutShell />
-      }
-    ],
-    error: <Error />
+    errorElement: <Error />
+  },
+  {
+    path: "/main",
+    element: <LayoutShell />,
+    errorElement: <Error />
   }
 ]);
