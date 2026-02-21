6bc4b9602f23e43280e14f9ef26fdb954903b319
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2026-02-21 23:59:21 +0530

    Refactored routing for the app.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import LayoutShell from "@/components/layout/LayoutShell";
 // import Intro from "../pages/Intro";
 // import Board from "@/components/canvas/Board";
@@ -6,9 +6,9 @@ import LayoutShell from "@/components/layout/LayoutShell";
 import Error from "../components/Error";
 
 export const router = createBrowserRouter([
-      {
-        path: "/",
-        element: <LayoutShell />,
-        error: <Error />
-      }
+  {
+    path: "/",
+    element: <LayoutShell />,
+    error: <Error />
+  }
 ]);
