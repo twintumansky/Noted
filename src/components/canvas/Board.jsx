fff499fb66825f9c483652650b39f586efe95256
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2026-03-01 18:13:39 +0530

    Implemented and completed the setup for the editor instance in the tldraw canvas component.

--- src/components/canvas/Board.jsx
+++ src/components/canvas/Board.jsx
@@ -1,4 +1,4 @@
-import { Tldraw } from 'tldraw'
+import { Tldraw, toRichText } from 'tldraw'
 import { useCanvas } from '@/context/CanvasContext'
 import 'tldraw/tldraw.css'
 
@@ -6,15 +6,31 @@ export default function Board() {
 
   const { setEditor } = useCanvas();
 
+  const handleMount = (editor) => {
+    setEditor(editor);
+    editor.updateInstanceState({ isGridMode: true });
+		editor.createShape({
+			type: 'text',
+			x: 200,
+			y: 200,
+			props: {
+				richText: toRichText('Hello world!'),
+			},
+		})
+
+		editor.selectAll()
+
+		editor.zoomToSelection({
+			animation: { duration: 5000 },
+		})
+	}
+
   return (
     <div className="absolute inset-0 overflow-hidden bg-[#fbfbfb]">
       <Tldraw
         hideUi={true}
         inferDarkMode={false}
-        onMount={(editor) => {
-          setEditor(editor);
-          editor.updateInstanceState({ isGridMode: true });
-        }}
+        onMount={handleMount}
       />
       <div className="absolute bottom-4 right-4 z-20 bg-white/80 backdrop-blur px-3 py-1.5 rounded-full border border-slate-200 text-xs font-medium text-slate-500 shadow-sm">
         100%
