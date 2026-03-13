0e9a85030ea1be9409b1900049bd1806546fdced
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2026-03-13 23:59:47 +0530

    Fixed the positioning of the popover element in the toolbar to sit perfectly besides the Add button.

--- src/components/canvas/Toolbar.jsx
+++ src/components/canvas/Toolbar.jsx
@@ -77,7 +77,7 @@ export default function Toolbar() {
             <HugeiconsIcon icon={Add01Icon} size={20} />
           </button>
         </PopoverTrigger>
-        <PopoverContent side='top ' align='start' className="absolute bottom-4 w-48 p-2 z-20 bg-white/80 backdrop-blur-md rounded-2xl shadow-sm border-slate-200">
+        <PopoverContent side='top ' align='end' sideOffset={12} className=" w-48 p-2 z-20 bg-white/80 backdrop-blur-md rounded-2xl shadow-sm border-slate-200">
           <div className='grid gap-1'>
             {shapesArray.map((shape) => {
               return (
